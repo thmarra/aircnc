ebbfa6179a48e2ad04b1038f6e85635fb268b75a
Thalita Marra <th-meneses@hotmail.com>
2019-10-03 21:55:30 -0300

    montagem tela de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+      <div className="content">
+        <p>Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
+        <form>
+          <label htmlFor="email">Email (*)</label>
+          <input type="email" id="email" placeholder="Seu melhor email"/>
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
